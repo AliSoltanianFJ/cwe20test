810633d51e2c643fa914d5d891c4ef7bbe9bbc73
Ali <86838752+AliSoltanianFJ@users.noreply.github.com>
2025-06-19 18:36:56 +1200

    Update scenario.py

--- scenario.py
+++ scenario.py
@@ -2,5 +2,4 @@ import os
 
 directory = input("Enter the directory to list: ")
 # run the command to list files in the directory
-command = f"ls -l {directory}"
-os.system(command)
+os.system(f"ls -l {directory}")
